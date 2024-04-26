1d22619990a3534a8b82980e65bc6bfd873daf93
Rogumann <lasse.pladsen@hotmail.com>
2024-04-26 15:21:03 +0200

    initial plot test

--- 3-body-simulation/src/App.js
+++ 3-body-simulation/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState} from "react";
+import Plot from "react-plotly.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [1, 4, 9],
+          },
+        ]}
+        layout={{ autosize: true }}
+      />
     </div>
   );
 }
